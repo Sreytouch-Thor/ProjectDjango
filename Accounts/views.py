19288211500b804cebf9b93ad3c0677532f0481e
sreytouch-thor <sreytouch.thor1234@gmail.com>
2023-10-19 17:16:54 +0700

    create Dynamic urls and forms

--- Accounts/views.py
+++ Accounts/views.py
@@ -6,9 +6,18 @@ from .models import *
 def home(request):
     customers = Customer.objects.all()
     orders = Order.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
     context = {
          'customers': customers,
          'orders' : orders,
+         'total_customers': total_customers,
+         'total_orders': total_orders,
+         'delivered': delivered,
+         'pending': pending,
     }
     return render(request,'Accounts/dashboard.html', context)
 
@@ -17,5 +26,18 @@ def products(request):
      
      return render(request,'Accounts/products.html', {'products': products})
 
-def customer(request):
-     return render(request,'Accounts/customer.html')
+def customer(request, pk):
+     customers = Customer.objects.get(id=pk)
+     orders = customers.order_set.all()
+     total_orders = orders.count()
+
+     context = {
+          'customers': customers,
+          'orders': orders,
+          'total_orders': total_orders,
+     }
+     return render(request,'Accounts/customer.html', context)
+
+def createOrder(request):
+
+     return render(request,'Accounts/order_form.html')
